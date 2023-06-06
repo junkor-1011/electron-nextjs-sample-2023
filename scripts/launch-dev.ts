9133a595209d71b0e1d1c0e29ad556033a920ea4
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-07 00:48:09 +0900

    dev launch script

--- scripts/launch-dev.ts
+++ scripts/launch-dev.ts
@@ -1,15 +1,25 @@
-import util from 'node:util';
-import childProcess from 'node:child_process';
-
-const exec = util.promisify(childProcess.exec);
+import { spawn } from 'node:child_process';
 
 function main() {
-  exec('pnpm dev:renderer')
-    .then((res) => { console.log(res) })
-    .catch((error) => { console.error(error) });
-  exec('pnpm dev:electron')
-    .then((res) => { console.log(res) })
-    .catch((error) => { console.error(error) });
+  // renderer
+  const rendererProc = spawn('pnpm', ['dev:renderer']);
+  console.log('renderer: ', rendererProc.pid);
+  rendererProc.stdout.on('data', (data) => {
+    console.log(data.toString());
+  })
+  rendererProc.stderr.on('data', (data) => {
+    console.log(data.toString());
+  })
+
+  // electron main
+  const mainProc = spawn('pnpm', ['dev:electron']);
+  console.log('main: ', mainProc.pid);
+  mainProc.stdout.on('data', (data) => {
+    console.log(data.toString());
+  })
+  mainProc.stderr.on('data', (data) => {
+    console.log(data.toString());
+  })
 }
 
 main()
