20dfabfb5dc521229b76ed55f3507cc46f7623b3
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-08 23:37:56 +0900

    csp for prod(using response header)

--- electron/index.ts
+++ electron/index.ts
@@ -2,7 +2,7 @@
 import { join } from 'node:path';
 
 // Packages
-import { BrowserWindow, app, ipcMain, type IpcMainEvent } from 'electron';
+import { BrowserWindow, app, ipcMain, session, type IpcMainEvent } from 'electron';
 import serve from 'electron-serve';
 import isDev from 'electron-is-dev';
 
@@ -12,6 +12,19 @@ const loadURL = serve({
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
+  // session
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    const cspContents = isDev
+      ? ["default-src 'self' 'unsafe-inline'"]
+      : ["default-src 'self'"];
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': cspContents,
+      }
+    });
+  });
+
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 900,
