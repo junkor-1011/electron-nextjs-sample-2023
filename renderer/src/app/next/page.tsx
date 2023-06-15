4a2aeb7e617995fcb73c9cd7192682e5224bddf9
junkor-1011 <tomoyukizxy@gmail.com>
2023-06-16 01:39:49 +0900

    ipc handler

--- renderer/src/app/next/page.tsx
+++ renderer/src/app/next/page.tsx
@@ -1,18 +1,17 @@
-import styles from './page.module.css'
+import SendButton from './SendButton';
+import InvokeButton from './InvokeButton';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.grid}>
-        <a
-          href="/"
-          className={styles.card}
-        >
-          <h2>
-            return to home
-          </h2>
+        <a href="/" className={styles.card}>
+          <h2>return to home</h2>
         </a>
+        <SendButton />
+        <InvokeButton />
       </div>
     </main>
-  )
+  );
 }
