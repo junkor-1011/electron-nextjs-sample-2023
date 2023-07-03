202df972cb0b73531ee23894c730f33e747dfc37
junkor-1011 <tomoyukizxy@gmail.com>
2023-07-04 02:33:17 +0900

    refactor: registerExampleEvent

--- electron/index.ts
+++ electron/index.ts
@@ -11,7 +11,7 @@ import {
   exampleChannel3,
 } from './lib/channels';
 import { invokeExampleHandler, sendExampleHandler } from './lib/handler';
-import { ListenerExampleEventArgs } from './lib/events';
+import { registerExampleEvent } from './lib/events';
 import { registerProtocol, protocolInfo } from './lib/custom-protocol';
 
 /** url of nextjs development server */
@@ -55,14 +55,7 @@ app.on('ready', async () => {
 
   const contents = mainWindow.webContents;
 
-  // send to renderer example
-  setInterval(() => {
-    const now = new Date();
-    const args: ListenerExampleEventArgs = [
-      `message from main: it is ${now.toISOString()}`,
-    ];
-    contents.send(exampleChannel3, ...args);
-  }, 10 * 1000);
+  registerExampleEvent({ contents, channel: exampleChannel3 });
 });
 
 // Quit the app once all windows are closed
--- electron/lib/events.ts
+++ electron/lib/events.ts
@@ -8,3 +8,20 @@ export type AddListenerExample = (
 ) => void;
 
 export type RemoveListenerExample = () => void;
+
+export const registerExampleEvent = ({
+  channel,
+  contents,
+}: {
+  channel: string;
+  contents: Electron.WebContents;
+}) => {
+  // send to renderer example
+  setInterval(() => {
+    const now = new Date();
+    const args: ListenerExampleEventArgs = [
+      `message from main: it is ${now.toISOString()}`,
+    ];
+    contents.send(channel, ...args);
+  }, 10 * 1000);
+};
